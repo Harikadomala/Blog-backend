90c8e9828a10ffc5d10ca988d713d3d617f96bb0
Harika Domala <harikadomala@gmail.com>
2025-07-25 20:49:21 +0530

    commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({ 
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'], 
+  origin: ['https://blog-frontend-1lj8.onrender.com/'], 
   credentials: true }));
 app.use(express.json());
 
